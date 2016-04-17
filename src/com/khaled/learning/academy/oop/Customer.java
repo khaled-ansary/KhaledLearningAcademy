3d3071707ba19bb3d832e4b2330f9e5ccd1080ba
khaled-ansary <ansary.development@gmail.com>
2016-04-17 17:18:47 +0200

    commit# Customer Class

---
+++ src/com/khaled/learning/academy/oop/Customer.java
@@ -0,0 +1,57 @@
+
+package com.khaled.learning.academy.oop;
+
+/**
+ *
+ * @author Khaled
+ * @website khaledlearningacademy.com
+ * @Cutomer Class
+ */
+public class Customer {
+    
+    private int customerID;
+    private String firstName;
+    private String lastName;
+    private String address;
+    private int phoneNumber;
+
+    public int getCustomerID() {
+        return customerID;
+    }
+
+    public void setCustomerID(int customerID) {
+        this.customerID = customerID;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public int getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(int phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }    
+}
